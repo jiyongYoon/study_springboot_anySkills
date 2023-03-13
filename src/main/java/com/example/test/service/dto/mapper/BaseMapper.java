fbc3dfe990e32a8b1d65d1206b5774816c68d9fc
jiyong.yoon <jyyoon0615@naver.com>
2023-03-13 19:27:42 +0900

    Mapper-1

---
+++ src/main/java/com/example/test/service/dto/mapper/BaseMapper.java
@@ -0,0 +1,15 @@
+package com.example.test.service.dto.mapper;
+
+import java.util.List;
+
+public interface BaseMapper<D, E> {
+
+    D toDto(E entity);
+
+    E toEntity(D dto);
+
+    List<D> toDtoList(List<E> entityList);
+
+    List<E> toEntityList(List<D> dtoList);
+
+}
---
+++ src/main/java/com/example/test/service/dto/mapper/SportsDtoMapper.java
@@ -0,0 +1,9 @@
+package com.example.test.service.dto.mapper;
+
+import com.example.test.model.Sports;
+import com.example.test.service.dto.SportsDto;
+
+
+public interface SportsDtoMapper extends BaseMapper<SportsDto, Sports> {
+
+}
